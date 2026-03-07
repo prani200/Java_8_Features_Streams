ba7e4913538e5215f4e0a98a97adc09244e041f9
Prani Sahith Kummari <pranisahith@gmail.com>
2026-03-07 15:16:31 +0530

    Update and rename EmployeeClass.java to EmployeeStreams/EmployeeClass.java

--- EmployeeStreams/EmployeeClass.java
+++ EmployeeStreams/EmployeeClass.java
@@ -1,15 +1,20 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+
 class Employee {
     private int id;
     private String name;
     private int age;
     private String department;
     private double salary;
-    public Employee(int id, String name, int age, String department, double salary) {}
+    public Employee(int id, String name, int age, String department, double salary) {
     this.id = id;
     this.name = name;
-    this.age age;
+    this.age = age;
     this.department = department;
     this.salary = salary;
+}
     public int getId() {
         return id;
     }
@@ -36,7 +41,5 @@ public class Main {
         List < Employee > employees = Arrays.asList(
             new Employee(1, "Surya", 25, "IT", 60000), new Employee(2, "Mahesh", 30, "HR", 50000), new Employee(3, "Kiran", 28, "IT", 75000), new Employee(4, "Anil", 35, "Finance", 90000), new Employee(5, "Suresh", 40, "IT", 120000), new Employee(6, "Ravi", 22, "HR", 40000), new Employee(7, "John", 29, "Finance", 85000), new Employee(8, "Sam", 31, "IT", 95000)
         );
-
-
     }
 }
